518a14d2d7b98fe523b0d01f6d0379516cd85f51
mordoss <milos8petrovic8@gmail.com>
2022-07-20 13:13:43 +0200

    add cover effects and change first post cover

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,34 +1,37 @@
-import cn from 'classnames'
-import Link from 'next/link'
+import cn from 'classnames';
+import Link from 'next/link';
 
 type Props = {
-  title: string
-  src: string
-  slug?: string
-}
+    title: string;
+    src: string;
+    slug?: string;
+};
 
 const CoverImage = ({ title, src, slug }: Props) => {
-  const image = (
-       <img
-        src={src}
-        alt={`Cover Image for ${title}`}
-        style={{margin: "auto"}}
-        className={cn('shadow-sm', {
-          'hover:shadow-lg transition-shadow duration-200 content-center': slug,
-        })}
-      />
-   )
-  return (
-    <div className="sm:mx-0">
-      {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a aria-label={title}>{image}</a>
-        </Link>
-      ) : (
-        image
-      )}
-    </div>
-  )
-}
+    const image = (
+        <img
+            src={src}
+            alt={`Cover Image for ${title}`}
+            style={{ margin: 'auto' }}
+            className={cn(
+                'shadow-sm hover:scale-105 transition:scale duration-500',
+                {
+                    'hover:shadow-lg transition-all duration-500': slug,
+                }
+            )}
+        />
+    );
+    return (
+        <div className="sm:mx-0">
+            {slug ? (
+                <Link as={`/posts/${slug}`} href="/posts/[slug]">
+                    <a aria-label={title}>{image}</a>
+                </Link>
+            ) : (
+                image
+            )}
+        </div>
+    );
+};
 
-export default CoverImage
+export default CoverImage;
